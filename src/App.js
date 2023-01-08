c4e1011dd27a9297d5858bd50a565c2be7be60e8
Nehir Çakılcı <nehir.cakilci@gmail.com>
2023-01-08 16:42:11 +0200

    updated the app js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my first workshop on github.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.archisacademy.com/tr"
           target="_blank"
           rel="noopener noreferrer"
         >
